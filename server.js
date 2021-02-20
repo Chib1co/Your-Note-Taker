6f0af9b045188a7484ba78bf6962c0f67033e9f2
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-02-20 16:13:17 +1100

    update server.js port is working

--- server.js
+++ server.js
@@ -5,19 +5,66 @@ const fs = require('fs');
  
 // Sets up the Express App
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+let noteData = [];
 
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, "/public")));
 
 
 // Basic route that sends the user first to the AJAX Page
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public','index.html')));
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+//api call  
+app.get('/api/notes', function(err, res){
+  try{
+    // reads the notes from json file
+    noteData = fs.readFileSync("./db/db.json", "utf8");
+    // parse it so notesData is an array of objects
+    noteData = JSON.parse(noteData);
+    console.log('working')
+
+  }catch(err){
+    console.log('error')
+  }
+  res.json(noteData)
+});
+
+
+// writes the new note to the json file
+app.post("/api/notes", function(req, res) {
+  try {
+    // reads the json file
+    noteData = fs.readFileSync("./db/db.json", "utf8");
+    console.log(noteData);
+
+    // parse the data to get an array of objects
+    noteData = JSON.parse(noteData);
+    // Set new notes id
+    req.body.id = noteData.length;
+    // add the new note to the array of note objects
+    noteData.push(req.body); // req.body - user input
+    // make it string(stringify)so you can write it to the file
+    noteData = JSON.stringify(noteData);
+    // writes the new note to file
+    fs.writeFile("./db/db.json", noteData, "utf8", function(err) {
+      // error handling
+      if (err) throw err;
+    });
+    // changeit back to an array of objects & send it back to the browser(client)
+    res.json(JSON.parse(noteData));
+
+    // error Handling
+  } catch (err) {
+    throw err;
+    console.error(err);
+  }
+});
 
 // Create New notes - takes in JSON input
 // app.post('/api/notes', (req, res) => {
